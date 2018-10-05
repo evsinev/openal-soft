3d4c1d247bb9db6f18b3ef225c3613c6d59316b9
Evgeniy Sinev <es@payneteasy.com>
2018-10-05 13:21:20 +0300

    Only xcode 10 build

--- build-osx.sh
+++ build-osx.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-set -eux
+#set -eux
 xcodebuild -version
 MACOSX_DEPLOYMENT_TARGET=10.8
 
@@ -9,3 +9,7 @@ cd build
 cmake -D LIBTYPE:STRING=STATIC -D CMAKE_OSX_DEPLOYMENT_TARGET:STRING=10.8 ..
 make -j4
 sudo make install
+
+cat /Users/distiller/project/build/CMakeFiles/CMakeOutput.log
+
+cat /Users/distiller/project/build/CMakeFiles/CMakeError.log
