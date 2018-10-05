28537af6672d4896ecb09ab34d8ab892f412295e
Evgeniy Sinev <es@payneteasy.com>
2018-10-05 13:42:58 +0300

    Moved to a minimum deployment target of OS X 10.9

--- build-osx.sh
+++ build-osx.sh
@@ -1,12 +1,12 @@
 #!/usr/bin/env bash
 #set -eux
 xcodebuild -version
-MACOSX_DEPLOYMENT_TARGET=10.8
+MACOSX_DEPLOYMENT_TARGET=10.9
 
 brew install cmake
 
 cd build
-cmake -D LIBTYPE:STRING=STATIC -D CMAKE_OSX_DEPLOYMENT_TARGET:STRING=10.8 ..
+cmake -D LIBTYPE:STRING=STATIC -D CMAKE_OSX_DEPLOYMENT_TARGET:STRING=10.9 ..
 make -j4
 sudo make install
 
